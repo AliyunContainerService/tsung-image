7e694d88cdc0258b5cbdd6f6aa3ff9c425b12f33
ringtail <zhongwei.lzw@alibaba-inc.com>
2016-09-07 13:01:26 +0800

    fix shell scripts

--- start.sh
+++ start.sh
@@ -6,16 +6,16 @@ fi
 
 mode="$1"
 
-if [ "$mode" == "single" ]; then
+if [ "$mode" = "single" ]; then
   tusng -k start
 fi
 
-if [ "$mode" == "master" ]; then
+if [ "$mode" = "master" ]; then
   /entry.sh /usr/sbin/sshd -d -f /etc/ssh/sshd_config;
   tsung -k start
 fi
 
-if [ "$mode" == "slave" ]; then
+if [ "$mode" = "slave" ]; then
   /entry.sh /usr/sbin/sshd -d -f /etc/ssh/sshd_config;
   top -b
 fi
