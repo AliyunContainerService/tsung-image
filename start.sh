0da011a3025cb752107fd2f28bce0a09be5cfb17
ringtail <zhongwei.lzw@alibaba-inc.com>
2016-09-07 13:03:30 +0800

    fix content

--- start.sh
+++ start.sh
@@ -11,11 +11,11 @@ if [ "$mode" = "single" ]; then
 fi
 
 if [ "$mode" = "master" ]; then
-  /entry.sh /usr/sbin/sshd -d -f /etc/ssh/sshd_config;
+  /entry.sh /usr/sbin/sshd -d -f /etc/ssh/sshd_config & >> /dev/null;
   tsung -k start
 fi
 
 if [ "$mode" = "slave" ]; then
-  /entry.sh /usr/sbin/sshd -d -f /etc/ssh/sshd_config;
+  /entry.sh /usr/sbin/sshd -d -f /etc/ssh/sshd_config & >> /dev/null;
   top -b
 fi
